7ebbab48209b5cf22f7f80d2acb81e498e96e442
Jean-Lionel <nijeanlionel@gmail.com>
2023-04-09 08:06:49 +0200

    add product

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductOveView extends StatefulWidget {
+  const ProductOveView({super.key});
+
+  @override
+  State<ProductOveView> createState() => _ProductOveViewState();
+}
+
+class _ProductOveViewState extends State<ProductOveView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
