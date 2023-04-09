fba3824ff1ff23be5e490ee05da042e089e04ff1
Jean-Lionel <nijeanlionel@gmail.com>
2023-04-09 09:42:02 +0200

    display shopping cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/products_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
        home: Scaffold(
         appBar: AppBar(title: Text("My Shop"),
         ),
-        body: Center(child: Text("My Shop"),),
+        body: ProductVeiwScreen(),
        ),
        );
   }
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,1 +1,16 @@
-class Product {}
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool? isFavorite;
+
+  Product(
+      {required this.id,
+      required this.title,
+      required this.price,
+      required this.description,
+      required this.imageUrl,
+      isFavorite});
+}
