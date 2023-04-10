494bfccdbd6e844f42a24b45b62d6676d04ce951
Jean-Lionel <nijeanlionel@gmail.com>
2023-04-10 05:20:32 +0200

    cart item

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class CartItem {
   final String id;
   final String title;
@@ -10,4 +12,27 @@ class CartItem {
     required this.price,
     required this.quantity,
   });
+}
+
+class Cart with ChangeNotifier {
+  final Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItems(productId, title, price) {
+    if (_items.containsKey(productId)) {
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: new DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+  }
 }
